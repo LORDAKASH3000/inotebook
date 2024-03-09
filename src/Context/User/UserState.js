c8d91a71961623c1c0d2dcb3f5cf683f837dc087
sayan3000 <sayanghosh16014@gmail.com>
2024-03-09 18:19:39 +0530

    modified:   package-lock.json 	modified:   src/App.js 	modified:   src/Context/User/UserState.js 	modified:   src/components/Authentication/SignOut.js 	modified:   src/components/Notes/NoteList.js

--- src/Context/User/UserState.js
+++ src/Context/User/UserState.js
@@ -2,15 +2,22 @@ import React from 'react'
 import UserContext from './UserContext';
 
 const UserState = (props)=>{
-
+    
     const auth_token = localStorage.getItem('auth-token');
 
     const signUp = ()=>{
         
     }
 
-    const signIn = ()=>{
-
+    const signIn = async ()=>{
+        e.preventDefault();
+        const response = await fetch(process.env.REACT_APP_SIGNIN_URL, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({email: credentials.email, password: credentials.password})
+        });
     }
 
     const fetchUserDetails = ()=>{
